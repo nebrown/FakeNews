59cd853377cff533c0dbd9d6a6e45e0599498005
Naylan Adre <nadre@ucsc.edu>
2018-02-12 03:25:29 -0800

    Added Button and Label

--- fnGUI.py
+++ fnGUI.py
@@ -4,10 +4,24 @@ from PyQt5 import QtWidgets
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
-    w = QtWidgets.QWidget()
-    w.setWindowTitle('Read FakeNooz')
-    w.show()
+    mainWindow = QtWidgets.QWidget()
+    mainWindow.setGeometry(100,100,500,300)
+
+
+    # create features
+    button1 = QtWidgets.QPushButton(mainWindow)
+    label1 = QtWidgets.QLabel(mainWindow)
+
+    # text for features
+    button1.setText('Search')
+    label1.setText('User Input for Articles')
+
+    # feature placement
+    label1.move(180,100)
+    button1.move(180,150)
+
+    mainWindow.setWindowTitle('Read FakeNooz')
+    mainWindow.show()
     sys.exit(app.exec_())
     
 window()
-
