b6e60f7ccc10caa101c9683d6b15c909a4e0e5ad
Naylan Adre <nadre@ucsc.edu>
2018-02-12 03:37:34 -0800

    Added Layouts

--- fnGUI.py
+++ fnGUI.py
@@ -7,7 +7,6 @@ def window():
     mainWindow = QtWidgets.QWidget()
     mainWindow.setGeometry(100,100,500,300)
 
-
     # create features
     button1 = QtWidgets.QPushButton(mainWindow)
     label1 = QtWidgets.QLabel(mainWindow)
@@ -17,9 +16,23 @@ def window():
     label1.setText('User Input for Articles')
 
     # feature placement
-    label1.move(180,100)
-    button1.move(180,150)
+    #label1.move(180,100)
+    #button1.move(180,150)
 
+    # container for labels
+    # horizontal
+    h_box = QtWidgets.QHBoxLayout()
+    h_box.addStretch()
+    h_box.addWidget(label1)
+    h_box.addStretch()
+
+    # vertical
+    v_box = QtWidgets.QVBoxLayout()
+    v_box.addWidget(button1)
+    v_box.addLayout(h_box)
+
+    # mainWindow settings
+    mainWindow.setLayout(v_box)
     mainWindow.setWindowTitle('Read FakeNooz')
     mainWindow.show()
     sys.exit(app.exec_())
