7dcaa0efaf02e874e3869bc7dd9170d8ec15ca05
jcbauman <35509918+jcbauman@users.noreply.github.com>
2018-02-21 00:02:01 -0800

    Add files via upload

---
+++ pulldown.py
@@ -0,0 +1,12 @@
+import newspaper
+
+#returns URLS of newspaper categories
+def pullDown(newspaperURL):
+	try:
+		paper = newspaper.build(newspaperURL)
+	except:
+		print("Cannot reach newspaper, invalid newspaper URL.")
+	return paper.category_urls()
+
+#tester
+#print(pullDown("https://www.cnn.com"))
