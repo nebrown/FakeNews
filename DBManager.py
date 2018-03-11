c4a7aece7d9d4459baaf4b2f6049d7fa99be395a
Noah Brown <nebrown@ucsc.edu>
2018-03-11 13:41:32 -0700

    Updated db added testing folder

--- DBManager.py
+++ DBManager.py
@@ -17,10 +17,10 @@ class DBManager:
     # initializes db connection on init
     def __init__(self, dbName):
         # self.dbName = dbNam
-        try:
-            os.remove(dbName+'.db')
-        except OSError:
-            pass
+        # try:
+        #     os.remove(dbName+'.db')
+        # except OSError:
+        #     pass
         self.conn = sqlite3.connect(dbName+'.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
     # Creates the database to store articles
@@ -46,6 +46,21 @@ class DBManager:
         score = data[5]
         self.conn.execute("INSERT INTO " + self.tableName + " (title, article, source, category, topicIdx, score) VALUES (?,?,?,?,?,?);", (title, article, source, category, topic, score))
 
+    # return all elements of a specified col
+    def getAll(self, colName):
+        c=self.conn.cursor()
+        c.execute("SELECT DISTINCT "+colName+" FROM "+self.tableName)
+        result = c.fetchall()
+        return result
+
+    # returns an article with id number from param
+    def getID(self, id):
+        # db is one indexed. add one to execute
+        c = self.conn.cursor()
+        c.execute("SELECT title, article, source, category FROM "+self.tableName+" WHERE id="+str(id +1))
+        result = c.fetchall()
+        return result[0]
+
     # dump contents out to terminal
     def printAll(self):
         c=self.conn.cursor()
@@ -56,6 +71,14 @@ class DBManager:
 # Test code
 # t = DBManager('test')
 # t.createTable()
-# t.add(('./', 'url', 'title', 'text', 0)
+# t.add(('title', 'article1', 'url', 'All', None, None))
+# t.add(('title', 'article2', 'url', 'All', None, None))
+# t.add(('title', 'article3', 'url', 'All', None, None))
+# t.add(('title', 'article4', 'url', 'All', None, None))
+# t.add(('title', 'article5', 'url', 'All', None, None))
+# t.add(('title', 'article6', 'url', 'All', None, None))
+# r = t.getID(2)
+# print(r[0][0])
+# pprint(r)
 # t.printAll()
 #t.test()
